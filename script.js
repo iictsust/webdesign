015ea4d815d72fc92efff9fb607374fed22e7270
iictsust <111361367+iictsust@users.noreply.github.com>
2022-12-19 15:32:29 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form');
+
+const input = document.getElementById('item');
+
+
+
+const btn_del_submit = document.getElementById('submit');
+
+const btn_del_clear = document.getElementById('clear');
+
+const btn_del_Savel all = document.getElementById('Save all');
+
+const btn_del_Window closed = document.getElementById('Window closed');
+
+
+
+
+const tbl = document.querySelector('table');
+
+
+
+
